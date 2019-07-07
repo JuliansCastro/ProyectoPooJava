55724616f9e288ccc68ff4e43ef14bab084eb98f
Julian Castro <juacastropa@unal.edu.co>
2019-07-07 09:02:21 -0500

    Creación de metodos de la clase ExportData, falta un poco para asociarlos a las otras clases

--- Anthropometric/src/logicBusiness/ExportData.java
+++ Anthropometric/src/logicBusiness/ExportData.java
@@ -1,5 +1,6 @@
-
 package logicBusiness;
+
+import java.io.*;
 
 /*
  * @author JULIAN C
@@ -7,5 +8,52 @@ package logicBusiness;
  * @author JUAN B
  */
 public class ExportData {
-    int i=0;
+
+    private BufferedReader bufferRead = null;
+
+    /* ----------- BUILDER ------------------------------------------------------------------------------------------------------*/
+    public ExportData() {
+    }
+
+    /* ----------- METHODS ------------------------------------------------------------------------------------------------------*/
+    public static void createFile(String nameDoc) {
+        FileWriter flwriter = null;
+        try {
+            flwriter = new FileWriter(nameDoc);
+            String pathfile = new File("").getAbsolutePath() + "\\" + nameDoc;
+            System.out.println("Archivo \'" + nameDoc + "\' creado satisfactoriamente en la ruta: \n" + pathfile);
+            //BufferedWriter bfwriter = new BufferedWriter(flwriter);
+            //bfwriter.close();
+        } catch (IOException e) {
+            System.out.print("Error, El archivo no Exite / No se puede leer el archivo");
+            e.printStackTrace();
+        } finally {
+            if (flwriter != null) {
+                try {//cierra el flujo principal
+                    flwriter.close();
+                } catch (IOException e) {
+                    System.out.print("Error, El archivo no Exite / No se puede leer el archivo");
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public String readTextLineDoc(String nameDoc) throws IOException {
+        String readTextLine = "";
+        try {
+            bufferRead = new BufferedReader(new FileReader(nameDoc));
+            readTextLine = bufferRead.readLine();
+            //System.out.println(readTextLine);
+        } catch (FileNotFoundException exp) {
+            System.out.println("Archivo no encontrado. Se ha creado uno nuevo.");
+            FileWriter fw = new FileWriter(nameDoc, true);
+        }
+        return readTextLine;
+    }
+
+    public void createReport() {
+    }
+
+    /* ----------- SETTERS & GETTERS --------------------------------------------------------------------------------------------*/
 }
