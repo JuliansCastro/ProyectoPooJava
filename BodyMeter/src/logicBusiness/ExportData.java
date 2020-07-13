f32d196fe2c73b7919be53533c7af3f156059d60
raochoa019 <raochoa@espol.edu.ec>
2020-07-12 22:57:10 -0500

    Changes that violated the Interface Segregation Principle

--- BodyMeter/src/logicBusiness/ExportData.java
+++ BodyMeter/src/logicBusiness/ExportData.java
@@ -17,8 +17,4 @@ public interface ExportData {
     public void saveData() throws Exception;
     public void deleteDataUser(String userToRemove) throws Exception;
     public void saveData(String user, String password, String id) throws Exception;
-    public byte[] encrypt(String unencrypted) throws Exception;
-    public String decrypt(String encrypted) throws Exception;
-    public Cipher getCipher(boolean toEncrypt) throws Exception;
-    public byte[] asBytes(String convertString);
 }
